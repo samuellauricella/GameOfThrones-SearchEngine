0e2b7a19901081b7bd5946c194fe34d9400a7e4e
Samuel <slauric1@gmail.com>
2017-03-28 13:22:24 -0400

    potential heroku fix

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,13 +1,6 @@
 class Character < ApplicationRecord
   validates :name, presence: true
   validates :culture, presence: true
-  validates :dateofbirth
-  validates :dateofdeath
-  validates :title
-  validates :house
-  validates :mother
-  validates :father
-  validates :spouse
 
   has_many :houses, through: :roles
 end
