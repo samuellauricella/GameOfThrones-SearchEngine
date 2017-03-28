48a27a23542bf592e5e698a14f68c61f9990e851
Samuel <slauric1@gmail.com>
2017-03-28 13:19:57 -0400

    potential heroku fix

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,29 +2,14 @@ class HousesController < ApplicationController
   before_action :set_house, only: [:update, :destroy]
 
 def index
-  render json {
-    houses: House.paginate(page: page)
-      .order(sort_by + ' ' + order)
-
-      page: page,
-      pages: House.pages
-  }
-
 end
 
 
 def search
-  query = params[:query]
-  houses = House.where('name LIKE ? OR place LIKE ? OR description LIKE ?',
-                       "%#{query}%", "%#{query}%", "%#{query}%")
-                .paginate(page: page)
-  render json: houses
 end
 
 private
-    ...
-    def page
-      params[:page] || 1
-    end
+  def page
+    params[:page] || 1
   end
 end
